a494769c5354c96f363efb7bea5560e8e6741979
Gautam <gk9765551@gmail.com>
2023-01-22 16:32:55 +0530

    types of Button

--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -39,14 +39,40 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text("App Bar"),
       ),
       body: SafeArea(
-        child: OutlinedButton(
-            onPressed: () {
-              print("Text Button Clicked");
-            },
-            onLongPress: () {
-              print("Button LongPresses");
-            },
-            child: Text("Text Button")),
+        child: Column(
+          children: [
+            Container(
+              child: OutlinedButton(
+                  onPressed: () {
+                    print("Outline Button Clicked");
+                  },
+                  onLongPress: () {
+                    print("Outline Button LongPresses");
+                  },
+                  child: Text("Text Button")),
+            ),
+            Container(
+              child: TextButton(
+                  onPressed: () {
+                    print("Text Button Clicked");
+                  },
+                  onLongPress: () {
+                    print("Button LongPresses");
+                  },
+                  child: Text("Text Button")),
+            ),
+            Container(
+                child: ElevatedButton(
+              onPressed: () {
+                print("Login btn pressed");
+              },
+              onLongPress: () {
+                print("Login btn longpress");
+              },
+              child: Text("Login"),
+            ))
+          ],
+        ),
       ),
     );
   }
