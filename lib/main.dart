4c2bf2150e5a6ac8c5d0f1767937181620c9be23
Divya7364 <divya7364sharma@gmail.com>
2023-02-05 00:37:10 +0530

    New Changes Made

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:fitorbit_app/Pages/login_page.dart';
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
-} 
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,15 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-      themeMode: ThemeMode.light,
-      theme: ThemeData(
-        
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      home: const TabBarDemo(),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
